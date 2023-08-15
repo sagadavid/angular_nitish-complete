4220a115a0fb97509d23d5f005828f1ff9fd3c9a
sagadavid <epostsaga@gmail.com>
2023-08-15 10:54:25 +0200

    style vs styleurl

--- workspace/projects/trialapp4workspace/src/app/path1/paht2/logut/logut.component.ts
+++ workspace/projects/trialapp4workspace/src/app/path1/paht2/logut/logut.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-logut',
-  templateUrl: './logut.component.html',
-  styleUrls: ['./logut.component.css']
+  //templateUrl: './logut.component.html',
+  template: `<h1>logut component : template vs templateUrl</h1>`,
+  //styleUrls: ['./logut.component.css'],
+  styles: ['h1{color:blue;font-size:15px;}'],
 })
-export class LogutComponent {
-
-}
+export class LogutComponent {}
