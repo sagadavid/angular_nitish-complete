61a4c07cccd465b99b370518f6fc07be62a50f0b
sagadavid <epostsaga@gmail.com>
2023-08-15 15:56:22 +0200

    event binding 1

--- startapp/src/app/app.component.ts
+++ startapp/src/app/app.component.ts
@@ -6,23 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'startapp';
   name: string = 'dave';
-  employees = ['elias', 'jon', 'sneeky'];
 
-  employX = {
-    name: 'eli',
-    salary: 120,
-    title: 'arkitekt',
-    src: 'https://images.unsplash.com/photo-1691698139354-201a6b38da1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNjJ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=400&q=60',
-    alt: 'profile photo',
-  };
-
-  getName(data?: number): string {
-    if (data) {
-      return 'hi data' + data;
-    } else {
-      return 'hi no data';
-    }
+  clickSave(): void {
+    alert('you saved it ! ');
   }
 }
--- workspace/projects/anylibrary/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of anylibrary
- */
-
-export * from './lib/anylibrary.service';
-export * from './lib/anylibrary.component';
-export * from './lib/anylibrary.module';
