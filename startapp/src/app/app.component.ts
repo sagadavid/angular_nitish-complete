9a18ef981de72c15cadf81279438ea3e778faf35
sagadavid <epostsaga@gmail.com>
2023-08-15 12:46:24 +0200

    text interpolation

--- startapp/src/app/app.component.ts
+++ startapp/src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'startapp';
+  name: string = 'dave';
+  employees = ['elias', 'jon', 'sneeky'];
+
+  employX = {
+    name: 'eli',
+    salary: 120,
+    title: 'arkitekt',
+    src: 'https://images.unsplash.com/photo-1691698139354-201a6b38da1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNjJ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=400&q=60',
+    alt: 'profile photo',
+  };
+
+  getName(data?: number): string {
+    if (data) {
+      return 'hi data' + data;
+    } else {
+      return 'hi no data';
+    }
+  }
 }
