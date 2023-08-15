70375fa6ccee1a9a867a8779ef66273da15bcc85
sagadavid <epostsaga@gmail.com>
2023-08-15 17:48:50 +0200

    class binding in array

--- startapp/src/app/app.component.ts
+++ startapp/src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   name: string = 'dave';
   isDisabled: boolean = false;
   txtId = 'sampleId';
-  customClass = 'farge storrelse';
+  customClass = ['farge', 'storrelse'];
 
   clickSave(): void {
     //alert('you saved it ! ');
