44797366530bd95b1fa6e9447426daa5ae99f254
sagadavid <epostsaga@gmail.com>
2023-08-16 13:32:32 +0200

    ngFor

--- startapp/src/app/app.component.ts
+++ startapp/src/app/app.component.ts
@@ -6,33 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name: string = 'dave';
-  isDisabled: boolean = true;
-  txtId = 'sampleId';
-  //customClass = { farge: this.isDisabled, storrelse: this.boo() };
-  src =
-    'https://images.unsplash.com/photo-1533228876829-65c94e7b5025?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80';
-  alt = 'unsplash photo';
-  width = 200;
-
-  bindendeStyle = 'color:green; font-style:italic';
-  bindendeStyle2 = { color: 'blue', 'font-style': 'italic' };
-
-  show = false;
-
-  clickSave(): void {
-    //alert('you saved it ! ');
-    this.name = 'name updated';
-  }
-
-  musOver(): void {
-    this.name = 'back to dave';
-  }
-
-  bytTxt(): void {
-    this.name = 'navn endring akseptert !';
-  }
-  boo() {
-    return true;
-  }
+  employees = ['jimmy', 'jonny', 'jenny', 'jacky'];
 }
