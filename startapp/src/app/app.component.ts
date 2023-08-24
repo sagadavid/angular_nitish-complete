b3807e808389a3fe99bab76af271c53dcbc1d1e3
sagadavid <epostsaga@gmail.com>
2023-08-24 23:06:50 +0200

    trackBy only on changed one-git fixed

--- startapp/src/app/app.component.ts
+++ startapp/src/app/app.component.ts
@@ -16,15 +16,6 @@ export class AppComponent {
 
   refresh(): void {
     this.employeesList = [
-      { name: 'feli', salary: '1300', location: 'uk' },
-      { name: 'keli', salary: '1100', location: 'usa' },
-      { name: 'sheli', salary: '700', location: 'sweden' },
-      { name: 'neli', salary: '1400', location: 'danmark' },
-      { name: 'emmet', salary: '3200', location: 'norway' },
-    ];
-  }
-  refresh(): void {
-    this.employeesList = [
       { name: 'weli', salary: '1300', location: 'uk' },
       { name: 'eli', salary: '1100', location: 'usa' },
       { name: 'deli', salary: '700', location: 'sweden' },
