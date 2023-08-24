a99020cb346f6af601dbc8604f670cb1a4867497
sagadavid <epostsaga@gmail.com>
2023-08-24 19:54:39 +0200

    ngFor trackBy changes

--- startapp/src/app/app.component.ts
+++ startapp/src/app/app.component.ts
@@ -13,4 +13,18 @@ export class AppComponent {
     { name: 'deli', salary: '700', location: 'sweden' },
     { name: 'reli', salary: '1400', location: 'danmark' },
   ];
+  refresh(): void {
+    this.employeesList = [
+      { name: 'weli', salary: '1300', location: 'uk' },
+      { name: 'eli', salary: '1100', location: 'usa' },
+      { name: 'deli', salary: '700', location: 'sweden' },
+      { name: 'reli', salary: '1400', location: 'danmark' },
+      { name: 'emmet', salary: '3200', location: 'norway' },
+    ];
+  }
+
+  trackEmpRefresh(index: number, employee: any): string {
+    return employee.name;
+    //thus, only the changed employee is refreshed. good for performance!
+  }
 }
